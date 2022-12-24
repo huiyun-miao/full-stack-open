e8951a62f9d1d74873135106b6aeec6de13f10af
Huiyun Miao <miaohy25@outlook.com>
2022-12-24 22:53:01 +0200

    Exercises 3.5-3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,6 @@
-const { request, response } = require('express')
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let persons = [
     { 
@@ -51,6 +51,39 @@ app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
   persons = persons.filter(p => p.id !== id)
   response.status(204).end()
+})
+
+const randomId = () => {
+  let id = Math.floor(Math.random() * 10000)
+  while (persons.includes(p => p.id === id)) {
+    id = Math.floor(Math.random() * 10000)
+  }
+  return id
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number is missing' 
+    })
+  }
+
+  if (persons.map(p => p.name).includes(body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    id: randomId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
